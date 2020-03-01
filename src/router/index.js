15df6114c02b5f18a83c56e061f7478c5ebdcd78
Chanchai Thaiyanon <chanchai@kkumail.com>
2020-03-01 18:37:31 +0700

    revert App.vue from previous version

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Test from '../views/Test.vue'
+import MasterConfig from '../views/MasterConfig.vue'
 
 Vue.use(VueRouter)
 
@@ -22,6 +23,10 @@ const routes = [
     path: '/test',
     name: 'Test',
     component: Test
+  }, {
+    path: '/MasterConfig',
+    name: 'MasterConfig',
+    component: MasterConfig
   }
 ]
 
